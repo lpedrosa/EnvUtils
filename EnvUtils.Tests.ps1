1f06c5741a6957e7e4824d4a1ba6e4fa646b0297
lpedrosa <2365589+lpedrosa@users.noreply.github.com>
2022-08-09 18:15:11 +0100

    expand vars by default
    ConvertFrom-Environment will try to expand all variables if possible
    with the following resolution order:
    
    * variables defined in the dotenv file
    * variables defined in the OS environment
    * default values if present e.g. ${I_DONT_EXIST:-default}
    
    It will not expand recursively, so if an expansion results in another
    expansion, it will stop expanding and print a warning.

--- EnvUtils.Tests.ps1
+++ EnvUtils.Tests.ps1
@@ -4,7 +4,7 @@ BeforeAll {
 
 Describe "ConvertFrom-Environment" {
     It "Should process a simple .env file" {
-        $environment = ConvertFrom-Environment .\fixtures\.simple.env
+        $environment = ConvertFrom-Environment "./fixtures/.simple.env"
 
         $environment.Count | Should -Be 2
         $environment["HELLO"] | Should -Be "WORLD"
@@ -12,9 +12,51 @@ Describe "ConvertFrom-Environment" {
     }
 
     It "Should not split values that have '=' in them" {
-        $environment = ConvertFrom-Environment .\fixtures\.complex-values.env
+        $environment = ConvertFrom-Environment "./fixtures/.complex-values.env"
 
         $environment.Count | Should -Be 1
         $environment["VAR2"] | Should -Be "My cool string=stuff"
+    }
+
+    It "Should expand variables by default" {
+        # set env before reading the file
+        $envExpansionValue = "from environment"
+        Set-Item -Path "Env:ENVUTILS_FROM_ENV" -Value $envExpansionValue
+
+        # silence warnings since there will be some vars that cannot be expanded
+        $environment = ConvertFrom-Environment "./fixtures/.expand-values.env" -WarningAction SilentlyContinue
+
+        $localExpansionValue = $environment["COOL_VALUE"]
+
+        $environment["VAR_TO_EXPAND_1"] | Should -Be $localExpansionValue
+        $environment["VAR_TO_EXPAND_2"] | Should -Be $localExpansionValue
+        $environment["VAR_TO_EXPAND_3"] | Should -Be $localExpansionValue
+        $environment["VAR_TO_EXPAND_4"] | Should -Be "$localExpansionValue||$localExpansionValue"
+
+        $environment["VAR_TO_EXPAND_DEFAULT"] | Should -Be "default"
+
+        $environment["VAR_TO_EXPAND_ENV_1"] | Should -Be $envExpansionValue
+        $environment["VAR_TO_EXPAND_ENV_2"] | Should -Be $envExpansionValue
+        $environment["VAR_TO_EXPAND_ENV_3"] | Should -Be $envExpansionValue
+
+        $environment["MULTIPLE_EXPANSIONS"] | Should -Be "$($localExpansionValue)::$($localExpansionValue)::default"
+
+        $environment["EDGE_CASE_1"] | Should -Be "$($localExpansionValue):-default"
+        $environment["EDGE_CASE_2"] | Should -Be "`$I_DO_NOT_EXIST:-default"
+
+        # set env back to normal
+        Remove-Item -Path "Env:ENVUTILS_FROM_ENV"
+    }
+
+    It "Should not expand variables if asked" {
+        'COOL_VALUE=nice nice
+        VAR_TO_EXPAND=$COOL_VALUE' | Tee-Object '.fake.env'
+        
+        $environment = ConvertFrom-Environment '.fake.env' -NoExpand
+
+        $localExpansionValue = $environment["COOL_VALUE"]
+        $environment["VAR_TO_EXPAND"] | Should -Not -Be $localExpansionValue
+
+        Remove-Item '.fake.env'
     }
 }
